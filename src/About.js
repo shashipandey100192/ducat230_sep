2a6011ff0f43fae10e09d8bf1c4070bf4dfd7ba9
windows <windows@microsoft.com>
2023-09-07 15:14:53 +0530

    any message

---
+++ src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Abc()
+{
+    return "welcome to react js";
+}
+
+export default Abc
+
+export function Abcd()
+{
+    return "this is name export function";
+}
+
+export function Xyz()
+{
+    return <p><h1>heading one</h1> <h2>sdfsdfhjsdhfjsdhfjh sjdhfjshd</h2></p>;
+    return <p><h1>heading one</h1> <h2>sdfsdfhjsdhfjsdhfjh sjdhfjshd</h2></p>;
+    
+}
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Ducat from './About';
+import { Abcd , Xyz} from './About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Ducat></Ducat>
+    <Abcd></Abcd>
+    <Xyz></Xyz>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
