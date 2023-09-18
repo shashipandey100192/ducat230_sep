af38ce0c37237410056579f618ac8396816db56d
windows <windows@microsoft.com>
2023-09-18 14:51:14 +0530

    new ins

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Ducat, { Contactus } from './About';
-import { Abcd , Xyz} from './About';
-import Myclasscop from './MyFomrs';
-import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import Mybs from './Mybs';
-import Myreactbs from './Myreactbs';
-import Uimetrial from './Uimetrial';
+import "./style.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Uimetrial/>
-    <Mybs/>
-    <Myreactbs/>
-    <Ducat></Ducat>
-    <Abcd></Abcd>
-    <Xyz></Xyz>
-    <Contactus/>
-    <Myclasscop/>
     
     
   </React.StrictMode>
